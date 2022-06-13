31e4ef894fae686543b57a23034c31addc146060
Scott White <scottw@aja.com>
2022-06-13 02:03:51 +0100

    another one

--- bin/installKernelRasPi.sh
+++ bin/installKernelRasPi.sh
@@ -1,4 +1,12 @@
+# Filename: installKernelRasPi.sh
+# Description: install custom kernel build on raspberry pi
+
 KERNEL=kernel7
+
+# Install the kernel modules
+cd $HOME/projects/linux && sudo make modules_install
+
+# Copy the kernel and device tree to the boot directory
 sudo cp $HOME/projects/linux/arch/arm/boot/dts/*.dtb /boot/
 sudo cp $HOME/projects/linux/arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
 sudo cp $HOME/projects/linux/arch/arm/boot/dts/overlays/README /boot/overlays/
